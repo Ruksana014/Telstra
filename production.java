97841f4e4cce95b53a1b5d6128061f4685c8f799
root <root@ip-172-31-42-185.ap-south-1.compute.internal>
2023-04-06 06:09:53 +0000

    g

---
+++ production.java
@@ -0,0 +1,15 @@
+class NumberToWordExample1   
+{  
+//user-defined static method that converts a number into words  
+static void numberToWords(char num[])  
+{  
+//determines the number of digits in the given number  
+int len = num.length;  
+//checks the given number has number or not  class NumberToWordExample1   
+{  
+//user-defined static method that converts a number into words  
+static void numberToWords(char num[])  
+{  
+//determines the number of digits in the given number  
+int len = num.length;  
+//checks the given number has number or not  
